38cab10796b5f535a6772f8a80cece766096e6c6
ChukharevIlya <chukharevowen@yandex.ru>
2022-08-15 16:02:56 +0300

    fixed play

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -33,9 +33,9 @@ public class Player {
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.get(game) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("Необходимо установить игру " + game.getTitle());
         }
         return playedTime.get(game);
     }
